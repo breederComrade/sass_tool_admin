f8d923f42cbf88edc73027705a61a85760f17ddf
王俊 <1871015627@qq.com>
2018-11-21 20:08:05 +0800

    update new readme

---
+++ src/components/Test.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Text extends React.Component {
+    render(){
+      return (
+        <div>
+          fuck text
+        </div>
+      )
+    }
+}
